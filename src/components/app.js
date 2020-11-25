5930bf1b1864aba978d22d3644c605ee5c209804
jasongrogan <62570239+JasonMGrogan@users.noreply.github.com>
2020-11-25 15:49:52 -0600

    Updated commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,15 +1,36 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import NavigationContainer from "./navigation/navigation-container";
+import Home from "./pages/home";
+import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Redux Router</h2>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <Router>
+          <div>
+          <h1>Jason Grogan Portfolio</h1>
+          <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+            </Switch>
+          </div>
+        </Router>
+
       </div>
     );
   }
