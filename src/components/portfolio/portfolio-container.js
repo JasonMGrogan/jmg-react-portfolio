15110048a4e32f0742d860fa247f84bd7c3d2696
jasongrogan <62570239+JasonMGrogan@users.noreply.github.com>
2020-12-29 13:38:35 -0600

    Latest updates

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioItem from './portfolio-item';
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
     constructor() {
@@ -9,12 +10,7 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio",
             isLoading: false,
-            data: [
-                {title: "Quip", category: "eCommerce" }, 
-                {title: "Eventbrite", category: "Scheduling" }, 
-                {title: "Ministry Safe", category: "Enterprise" },
-                {title: "SwingAway", category: "eCommerce" }
-            ]
+            data: []
         };
 
         this.handleFilter = this.handleFilter.bind(this);
@@ -25,14 +21,30 @@ export default class PortfolioContainer extends Component {
             data: this.state.data.filter(item => {
                 return item.category === filter;
             })
-        })
+        });
     }
 
-    portfolioItems() {
-    
+    getPortfolioItems() {
+        axios
+          .get("https://jasonmgrogan.devcamp.space/portfolio/portfolio_items")
+          .then(response => {
+            this.setState({
+                data: response.data.portfolio_items
+            })
+          })
+          .catch(error => {
+            console.log(error);
+          });
+    }
+
+    portfolioItems() { 
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
+            return <PortfolioItem key={item.id} item={item} />;
         });
+    }
+
+    componentDidMount() {
+        this.getPortfolioItems();
     }
 
     render() {
@@ -40,17 +52,16 @@ export default class PortfolioContainer extends Component {
             return <div>Loading...</div>;
         }
 
-        return (
-            <div>
-                <h2>{this.state.pageTitle}</h2>
+        return ( 
 
-                <button onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
-                <button onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
-                <button onClick={() => this.handleFilter("Enterprise")}>Enterprise</button>
-
+            <div className="portfolio-items-wrapper">
+                <button className="btn" onClick={() => this.handleFilter("eCommerce")}>eCommerce</button>
+                <button className="btn" onClick={() => this.handleFilter("Scheduling")}>Scheduling</button>
+                <button className="btn" onClick={() => this.handleFilter("Enterprise")}>Enterprise</button>
                 {this.portfolioItems()}
-
             </div>
+
+            
         );
     }
 }
